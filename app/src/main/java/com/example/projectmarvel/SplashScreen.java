7a1caf18c261da88bb81e65d0c6587a2ea6743f5
Jusup <101665754+JusupBusurmankulov@users.noreply.github.com>
2022-07-08 17:42:43 +0600

    add to SplashScreen

---
+++ app/src/main/java/com/example/projectmarvel/SplashScreen.java
@@ -0,0 +1,32 @@
+package com.example.projectmarvel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+
+public class SplashScreen extends AppCompatActivity {
+    private void showCustomUI() {
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        showCustomUI();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                finish();
+            }
+        }, 1000);
+    }
+}
